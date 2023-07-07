24f618e8fdcbe3c4a3988b4c75c90ef90b045870
g_vl_v <gurin.vv27@gmail.com>
2023-07-07 14:22:20 +0300

    Add merged_data.csv

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import pandas as pd
 
-# Загрузка данных из CSV файлов
 data = pd.read_csv('data.csv')
 bacterial_descriptors = pd.read_csv('bacterial_descriptors.csv')
 drug_descriptors = pd.read_csv('drug_descriptors.csv')
 
-# Объединение датасетов по общим столбцам
 merged_data = pd.merge(data, bacterial_descriptors, on='Bacteria', how='left')
 merged_data = pd.merge(merged_data, drug_descriptors, on='drug', how='left')
 
-# Вывод объединенного датасета
 merged_data.to_csv('merged_data.csv', index=False)
 print(merged_data.head())
