d5133c71c47f5f5b1e89ea1fd151d334a1716a15
g_vl_v <gurin.vv27@gmail.com>
2023-07-07 17:25:13 +0300

    Add comment

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from itertools import combinations
 
+"""Объединение датасетов и загрузка из других баз"""
 data = pd.read_csv('data.csv')
 bacterial_descriptors = pd.read_csv('bacterial_descriptors.csv')
 drug_descriptors = pd.read_csv('drug_descriptors.csv')
@@ -13,21 +14,8 @@ drug_descriptors = pd.read_csv('drug_descriptors.csv')
 merged_data = pd.merge(data, bacterial_descriptors, on='Bacteria', how='left')
 merged_data = pd.merge(merged_data, drug_descriptors, on='drug', how='left')
 
-merged_data.to_csv('merged_data.csv', index=False)
-
-print(merged_data.info())
-print(merged_data.isnull().mean()*100)
-
-smiles = merged_data['smiles'].tolist()
-mol_objects = []
-for smi in smiles:
-    if isinstance(smi, str):
-        mol_objects += [Chem.MolFromSmiles(smi)]
-    else:
-        mol_objects += [None]
-
-
 def calc_rdkit_descriptors(s, mol_objects):
+    """Получение дескрипторов из rdkit по smiles"""
     res = []
     for mol in mol_objects:
         if mol is not None:
@@ -36,6 +24,14 @@ def calc_rdkit_descriptors(s, mol_objects):
             res += [np.NaN]
     return res
 
+smiles = merged_data['smiles'].tolist()
+mol_objects = []
+
+for smi in smiles:
+    if isinstance(smi, str):
+        mol_objects += [Chem.MolFromSmiles(smi)]
+    else:
+        mol_objects += [None]
 
 mw = calc_rdkit_descriptors('Descriptors.MolWt', mol_objects)
 logp = calc_rdkit_descriptors('Descriptors.MolLogP', mol_objects)
@@ -43,21 +39,21 @@ hba = calc_rdkit_descriptors('Descriptors.NumHAcceptors', mol_objects)
 hbd = calc_rdkit_descriptors('Descriptors.NumHDonors', mol_objects)
 tpsa = calc_rdkit_descriptors('Descriptors.TPSA', mol_objects)
 
-
 merged_data['Molecular_Weight'] = mw
 merged_data['LogP'] = logp
 merged_data['HBA'] = hba
 merged_data['HBD'] = hbd
 merged_data['TPSA'] = tpsa
 
-merged_data.to_csv('update_rdkit_merged_data.csv', index=False)
+merged_data.to_csv('merged_data.csv', index=False)
 
-print('-----------------------------')
+
+"""Анализ данных, построение графиков"""
 print(merged_data.info())
 print(merged_data.isnull().mean()*100)
-
 
 numeric_data = merged_data.select_dtypes(include='number')
 correlation_matrix = numeric_data.corr()
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
 plt.show()
+
