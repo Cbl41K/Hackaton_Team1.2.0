278f21ef089713b87ade8aa0bedd9a3839ce558c
g_vl_v <gurin.vv27@gmail.com>
2023-07-09 19:20:10 +0300

    Финиш

--- main.py
+++ main.py
@@ -5,30 +5,22 @@ from rdkit.Chem import Descriptors
 from sklearn.pipeline import make_pipeline
 import graphs
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn import preprocessing
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.svm import SVR
-from sklearn.neural_network import MLPRegressor
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
-
-
-
+from sklearn.model_selection import cross_val_score
+import pickle
 
 
 def merge_and_load_datasets(name_1, name_2, name_3):
     """
     Объединяет датасеты и загружает данные из других баз.
-
     Args:
         name_1, name_2, name_3 (str): Имена CSV-файлов (датасетов).
-
     Returns:
-        pandas.DataFrame: Объединенный датасет.
+        merged_data (pandas.DataFrame): Объединенный датасет.
     """
     data = pd.read_csv(name_1)
     bacterial_descriptors = pd.read_csv(name_2)
@@ -43,13 +35,11 @@ def merge_and_load_datasets(name_1, name_2, name_3):
 def calculate_rdkit_descriptors(s, mol_objects):
     """
     Вычисляет дескрипторы RDKit для заданных молекул.
-
     Args:
         s (str): Имя дескриптора для вычисления.
         mol_objects (list): Список объектов молекул.
-
     Returns:
-        list: Список значений дескрипторов.
+        descriptors (list): Список значений дескрипторов.
     """
     descriptors = []
     for mol in mol_objects:
@@ -64,12 +54,10 @@ def calculate_rdkit_descriptors(s, mol_objects):
 def process_data(data):
     """
     Обрабатывает и очищает базу данных.
-
     Args:
         data (pandas.DataFrame): Объединенный датасет.
-
     Returns:
-        pandas.DataFrame: Обработанный датасет.
+        data (pandas.DataFrame): Обработанный датасет.
     """
     # Преобразование значений в столбце 'gram' на числовые (0 и 1)
     data['gram'] = data['gram'].replace({'n': 0, 'p': 1})
@@ -135,15 +123,12 @@ def process_data(data):
 def load_and_process_drugbank(name, data):
     """
     Загружает данные из одного датасета и объединяет со вторым.
-
     Args:
         name (str): Имя CSV-файла (датасета).
         data (pandas.DataFrame): Другой датасет, содержащий столбец "drug".
-
     Returns:
-        pandas.DataFrame: Объединенный и обработанный датасет.
+        data (pandas.DataFrame): Объединенный и обработанный датасет.
     """
-
     drugbank = pd.read_csv(name)
 
     # Замена значений в столбце "name"
@@ -176,7 +161,6 @@ def load_and_process_drugbank(name, data):
 def analyze_data(data):
     """
     Анализирует данные и строит графики.
-
     Args:
         data (pandas.DataFrame): Обработанный датасет.
     """
@@ -191,17 +175,21 @@ def analyze_data(data):
 def save_data(data, name = 'data_new.csv'):
     """
     Сохраняет обработанные данные в CSV-файл.
-
     Args:
         data (pandas.DataFrame): Обработанный датасет.
         name (str): Имя CSV-файла для сохранения.
     """
-    # Сохранение обработанных данных в CSV-файле
     data.to_csv(name, index=False)
 
 
 def data_for_model(data):
-    # Удаление ненужных столбцов
+    """
+    Обрабатывает данные для обучения модели
+    Args:
+        data (pandas.DataFrame): Датасет.
+    Returns:
+        data (pandas.DataFrame): Обработанный датасет.
+    """
     columns = ['Drug_dose', 'NP_concentration', 'NumHAcceptors', 'NumHDonors',
                'TPSA', 'cal_pka_basic', 'cal_number_of_rings', 'Bacteria', 'NP_Synthesis',
                'drug', 'Drug_class_drug_bank', 'shape', 'method', 'isolated_from',
@@ -217,36 +205,98 @@ def data_for_model(data):
 
 
 def training_model(data):
-    # Разделение на признаки (X) и целевую переменную (y)
+    """
+    Обучает модель, выводит метрики (RMSE и R-squared (R^2) score) и строит график "feature importances".
+    Args:
+        data (pandas.DataFrame): Датасет.
+    Returns:
+        pipeline (sklearn.pipeline.Pipeline): Обученная модель.
+    """
     X = data.drop('ZOI_drug_NP', axis=1)
     y = data['ZOI_drug_NP']
 
-    # Разделение на обучающую и тестовую выборки
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Создание пайплайна с заполнением пропущенных значений и линейной регрессией
+    # Создание пайплайна с заполнением пропущенных значений
     pipeline = make_pipeline(
-        SimpleImputer(strategy='median'),
+        SimpleImputer(strategy='mean'),
         GradientBoostingRegressor()
     )
 
-    # Обучение пайплайна на обучающих данных
     pipeline.fit(X_train, y_train)
 
-    # Предсказание значений на тестовом наборе данных
     y_pred = pipeline.predict(X_test)
 
-    # Вычисление корня из среднеквадратичной ошибки (RMSE)
+    # Вывод метрик
     rmse = mean_squared_error(y_test, y_pred, squared=False)
     print("Root Mean Squared Error (RMSE):", rmse)
-
-    # Вычисление коэффициента детерминации R^2
     r2 = r2_score(y_test, y_pred)
     print("R-squared (R^2) score:", r2)
 
+    # Вывод кросс-валидации
+    scores = cross_val_score(pipeline, X, y, cv=3)
+    print('cross_val_score:', -scores.mean())
+
+    feature_importance = pipeline.named_steps['gradientboostingregressor'].feature_importances_
+
+    # Сортировка значимости признаков
+    sorted_indices = np.argsort(feature_importance)
+    sorted_feature_importance = feature_importance[sorted_indices]
+    sorted_features = X_train.columns[sorted_indices]
+
+    # График значимости признаков
+    plt.figure(figsize=(10, 6))
+    plt.barh(range(len(sorted_feature_importance)), sorted_feature_importance, align='center')
+    plt.yticks(range(len(sorted_feature_importance)), sorted_features)
+    plt.xlabel('Значимость признака')
+    plt.ylabel('Признак')
+    plt.title('Значимость признаков в модели')
+    plt.show()
+
+    return pipeline
+
+
+def save_model(model, filename = 'model_weights.pkl'):
+    """
+    Сохраняет обученную модель в файл для дальнейшего использования.
+    Args:
+         model (sklearn.pipeline.Pipeline): Обученная модель.
+    """
+    with open(filename, 'wb') as file:
+        pickle.dump(model, file)
+
+
+def load_model(filename = 'model_weights.pkl'):
+    """
+    Загружает обученную модель.
+    Args:
+         filename (str): Путь к файлу.
+    Returns:
+        model (sklearn.pipeline.Pipeline): Обученная модель.
+    """
+    with open(filename, 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+
+def predict_ZOI_drug_NP(data, model):
+    """
+    Предсказывает ZOI_drug_NP в представленном датасете.
+    Args:
+        data (pandas.DataFrame): Датасет.
+        model (sklearn.pipeline.Pipeline): Обученная модель.
+    Returns:
+        predicted_values (pandas.DataFrame): Предсказания модели.
+    """
+    features = data[['NP size_avg', 'ZOI_drug', 'ZOI_NP', 'fold_increase_in_antibacterial_activity (%)',
+                     'MDR_check', 'gram', 'biosafety_level', 'MolWt', 'MolLogP',
+                     'cal_logs', 'cal_pka_acidic', 'cal_rule_of_five']]
+    predicted_values = model.predict(features)
+
+    return predicted_values
+
 
 def main():
-    # Объединение датасетов и загрузка данных
     data = merge_and_load_datasets('data.csv', 'bacterial_descriptors.csv', 'drug_descriptors.csv')
 
     # Получение SMILES-строк и объектов молекул
@@ -260,25 +310,26 @@ def main():
         descriptor_values = calculate_rdkit_descriptors(descriptor, mol_objects)
         data[descriptor.split('.')[-1]] = descriptor_values
 
-    # Обработка и очистка данных
     data = process_data(data)
 
-    # Загрузка новых данных
     data = load_and_process_drugbank('drugbank.csv', data)
 
-    # Сохранение данных
     save_data(data, 'merged_data.csv')
 
-    # Анализ данных и построение графиков
     #analyze_data(data)
 
-    # Очистка базы для модели
     data = data_for_model(data)
 
     # Разделение датасета для обучения модели по значению таргета
     data, data_ZOI_drug_NP_Nan = data[data['ZOI_drug_NP'].notna()], data[data['ZOI_drug_NP'].isna()]
 
-    training_model(data)
+    model = training_model(data)
+    save_model(model)
+    model = load_model()
+
+    # Пример использования модели
+    print('Предсказания для данных:', predict_ZOI_drug_NP(data, model))
+
 
 if __name__ == '__main__':
     main()
